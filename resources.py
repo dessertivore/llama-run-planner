b3ae0f8e4967ded82e197b1d44623a6c8f0510db
dessertivore <landau.sophie@gmail.com>
2024-06-30 18:07:53 +0100

    get web scraping to remove tags properly, get a bit further with rag

--- resources.py
+++ resources.py
@@ -2,14 +2,22 @@ from typing import Iterator
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import ollama
 from bs4 import BeautifulSoup, SoupStrainer
+from langchain_community.llms.ollama import Ollama
 
 import requests
+from langchain_community.embeddings import OllamaEmbeddings
 
-# from langchain import hub
-# from langchain_chroma import Chroma
-# from langchain_core.output_parsers import StrOutputParser
-# from langchain_core.runnables import RunnablePassthrough
-# from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain import hub
+from langchain_chroma import Chroma
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+my_llm = Ollama(
+    model="gemma:2b",
+    temperature=0.1,
+)
 
 
 def create_plan(weekly_kilometres: int, weekly_runs: int):
@@ -33,36 +41,46 @@ def create_plan(weekly_kilometres: int, weekly_runs: int):
 
 # (create_plan(40, 3))
 
-# Load, chunk and index the contents of the blog.
+# Load content of blog
 html_doc = requests.get(
-    "https://www.runnersloveyoga.com/new-blog/2021/2/9/how-to-create-your-own-training-plan-with-a-printable-pdf"
+    "https://run.outsideonline.com/training/rules-when-setting-up-weekly-running-schedule/"
 )
 soup = BeautifulSoup(
     html_doc.text,
     "html.parser",
-    parse_only=SoupStrainer("p", attrs={"style": "white-space:pre-wrap;"}),
+    parse_only=SoupStrainer(
+        "article",
+        attrs={"class": "l-container l-article l-article--post u-spacing--triple"},
+    ),
 )
-soup_str = str(soup)
 
+# Get rid of tags
+soup = soup.text.replace("\n", " ")
+
+# Chunk text content
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_text(soup_str)
-print(splits)
-# vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+splits = text_splitter.split_text(soup)
+
+# Index text content
+vectorstore = Chroma.from_texts(
+    texts=splits, embedding=OllamaEmbeddings(model="gemma:2b")
+)
 
 # # Retrieve and generate using the relevant snippets of the blog.
-# retriever = vectorstore.as_retriever()
-# prompt = hub.pull("rlm/rag-prompt")
+retriever = vectorstore.as_retriever()
+prompt = "You are a running coach for amateur runners. Your running plans contain at least 1 tempo or interval run and 1 long run (distance for this should be 50%\ or less of weekly mileage)."
 
 
-# def format_docs(docs):
-#     return "\n\n".join(doc.page_content for doc in docs)
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 
-# rag_chain = (
-#     {"context": retriever | format_docs, "question": RunnablePassthrough()}
-#     | prompt
-#     | llm
-#     | StrOutputParser()
-# )
-
-# rag_chain.invoke("What is Task Decomposition?")
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | my_llm
+    | StrOutputParser()
+)
+for chunk in rag_chain.stream("Please give me a 40mpw running plan?"):
+    print(chunk, end="", flush=True)
+print(rag_chain.invoke("Please give me a 40mpw running plan?"))
